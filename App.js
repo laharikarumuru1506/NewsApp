ed696f6aced87dc293886e53b53c4950f180ac15
Lahari Karumuru <77681081+laharikarumuru1506@users.noreply.github.com>
2022-08-03 16:47:48 +0530

    Add existing project files prior to the push to Github.

--- App.js
+++ App.js
@@ -1,20 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from 'react-navigation-stack';
+import HomeScreen from './src/Screens/HomeScreen';
+import GetNews from './src/Screens/GetNews';
+import WebViewComponent from './src/Components/WebView';
 
-export default function App() {
+const Stack = createStackNavigator();
+
+function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Trending" component={HomeScreen} />
+        <Stack.Screen name='GetNews' component={GetNews} />
+        <Stack.Screen name='WebView' component={WebViewComponent} options={{ headerShown: false }} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
